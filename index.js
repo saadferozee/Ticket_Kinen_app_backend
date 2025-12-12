d47cee375b29c8559783c615641786fa40ede365
saadferozee <saadferozee@gmail.com>
2025-12-13 03:13:09 +0600

    added role finding functionalities

---
+++ index.js
@@ -0,0 +1,80 @@
+require('dotenv').config()
+
+// Import all dependencies
+const express = require('express')
+const cors = require('cors')
+
+// Create app and configure middleware
+const app = express()
+app.use(cors({
+    origin: [
+        'http://localhost:5173',
+        'http://localhost:5174'
+    ],
+    credentials: true
+}))
+app.use(express.json())
+
+// Defining ports
+const port = process.env.PORT || 3568
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@ticketkinen-app.zzmy5yu.mongodb.net/?appName=ticketkinen-app`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+
+
+        // My code starts from here
+
+        app.get('/', (req, res) => {
+            res.send('Welcome to Ticket-kinen App database.')
+        })
+
+        const database = client.db('ticket-kinen-app')
+        const users = database.collection('all-users')
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            user.role = 'user'
+            user.createdAt = new Date()
+            const result = await users.insertOne(user)
+            res.send(result)
+        })
+        app.get('/users', async (req, res) => {
+            const result = await users.find().toArray()
+            res.send(result)
+        })
+        app.get('/users/role/:email', async (req, res) => {
+            const { email } = req.params
+            const query = { email: email }
+            const result = await users.findOne(query)
+            res.send(result.role)
+        })
+
+        app.listen(port, () => {
+            console.log(`server is running on Port: ${port}`)
+        })
+
+
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
