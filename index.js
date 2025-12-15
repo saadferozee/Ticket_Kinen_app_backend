a738e990e216b99feffd9382309fb8af80b558ff
saadferozee <saadferozee@gmail.com>
2025-12-15 22:24:59 +0600

    version-3.0.0, added Firebase token security on private routes

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require('dotenv').config()
 // Import all dependencies
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const admin = require('firebase-admin')
 
 // Create app and configure middleware
 const app = express()
@@ -15,11 +17,33 @@ app.use(cors({
 }))
 app.use(express.json())
 
+// Admin middleware creation for Security
+const decoded = Buffer.from(process.env.FIREBASE_KEY, 'base64').toString('utf8')
+const serviceAccount = JSON.parse(decoded);
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+const verifyFirebaseToken = async (req, res, next) => {
+    const token = req.headers.authorization
+    if (!token) {
+        return res.status(401).send({errorCode: 401, message: 'Unauthorized Access'})
+    }
+    try {
+        const idToken = token.split(' ')[1]
+        const decoded = await admin.auth().verifyIdToken(idToken)
+        req.decoded_email = decoded.email
+        next()
+    }
+    catch{
+        return res.status(401).send({ errorCode: 401, message: 'Unauthorized Access'})
+    }
+}
+
 // Defining ports
 const port = process.env.PORT || 3568
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@ticketkinen-app.zzmy5yu.mongodb.net/?appName=ticketkinen-app`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -56,7 +80,7 @@ async function run() {
             const result = await users.insertOne(user)
             res.send(result)
         })
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyFirebaseToken, async (req, res) => {
             const result = await users.find().toArray()
             res.send(result)
         })
@@ -71,7 +95,7 @@ async function run() {
         })
         app.get('/users/role/:email', async (req, res) => {
             const { email } = req.params
-            const query = { email: email }
+            const query = { email }
             const result = await users.findOne(query)
             if (!result) {
                 return res.send(false);
@@ -90,7 +114,25 @@ async function run() {
             const result = await tickets.find().toArray()
             res.send(result)
         })
+        app.get('/tickets/ticket/:id', async (req, res) => {
+            const {id} = req.params
+            const query = {_id: new ObjectId(id)}
+            const result = await tickets.findOne(query)
+            res.send(result)
+        })
+        app.get('/tickets/my-tickets/:email', async (req, res) => {
+            const { email } = req.params
+            const query = { vendorEmail: email }
+            const result = await tickets.find(query).toArray()
+            res.send(result)
+        })
 
+        // Error
+        // app.get('/*', (req, res) => {
+        //     console.log('Nothing, 404')
+        //     res.send('Nothing is here, 404 Error!')
+        // })
+        // Listener
         app.listen(port, () => {
             console.log(`server is running on Port: ${port}`)
         })
